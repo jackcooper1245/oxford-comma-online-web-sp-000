69984ea2ccd5ade3b0e172a5c3a97ad5d73877c0
Jack Cooper <jackcooper1245@gmail.com>
2020-04-07 15:24:06 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -3,7 +3,8 @@ if array.length == 1
   return array[0]
 elsif array.length == 2
   return array.join(" and ")
-else
-  return array.join(",")
+else 
+  array[-1] = "and " + array[-1]
+  return array.join(", ")
 end
 end
